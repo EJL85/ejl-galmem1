cf653276fe0319a8dc4ae08684e8a990b8677be0
CJ R <cj@null.computer>
2018-02-22 15:41:40 -0700

    :neckbeard: Update version-3 express app to be a better example for students

--- drills/version-3/app.js
+++ drills/version-3/app.js
@@ -1,44 +1,28 @@
 const express = require("express");
-const app = express();
-const queries = require("./queries");
 const bodyParser = require("body-parser");
+
+const app = express();
+
+const coffees = require("./routes/games");
 
 app.use(bodyParser.json());
 
-app.get("/games", (request, response) => {
-    queries.list().then(games => {
-        response.json({games});
-    }).catch(console.error);
+app.use("/games", coffees);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
-app.get("/games/:id", (request, response) => {
-    queries.read(request.params.id).then(game => {
-        game
-            ? response.json({game})
-            : response.sendStatus(404)
-    }).catch(console.error);
-});
-
-app.post("/games", (request, response) => {
-    queries.create(request.body).then(game => {
-        response.status(201).json({game});
-    }).catch(console.error);
-});
-
-app.delete("/games/:id", (request, response) => {
-    queries.delete(request.params.id).then(() => {
-        response.sendStatus(204);
-    }).catch(console.error);
-});
-
-app.put("/games/:id", (request, response) => {
-    queries.update(request.params.id, request.body).then(game => {
-        response.json({game});
-    }).catch(console.error);
-});
-
-app.use((request, response) => {
-    response.send(404);
+// error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: req.app.get("env") === "development" ? err.stack : {}
+    });
 });
 
 module.exports = app;
